7158733e62ea59038e28dfb8068b4f73eeb7cd47
p22rstewart <p22rstewart@fairfieldprep.org>
2021-12-09 10:39:51 -0500

    .

---
+++ hw8-1-1.py
@@ -0,0 +1,28 @@
+# Author RTS 12/9/21
+
+def grade (choice):
+    if choice < 60:
+        print("F")
+    elif choice <= 64:
+        return("D")
+    elif choice <= 69:
+        return("D+")
+   elif choice <= 72:
+        return("C-")
+    elif choice <= 76:
+        return("C")
+    elif choice <= 79:
+        return("C+")
+    elif choice <= 82:
+        return("B-")
+    elif choice <= 86:
+        return("B")
+    elif choice <= 89:
+        return("B+")
+    elif choice <= 92:
+        return("A-")
+    elif choice <= 100:
+        return("A")
+
+choice = int(input("Enter your Grade: "))
+print(grade)
